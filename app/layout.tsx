cad56ca1d16aa13474a12acc66c4936b882b375a
Arian <ariankab338@gmail.com>
2026-08-21 23:47:07 +0600

    layout changed to remove navbar temorarily

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { Providers } from './providers';
-import Navbar from '@/components/NavBar';
+// import Navbar from '@/components/NavBar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -22,7 +22,7 @@ export default function RootLayout({
    <html lang="en">
       <body className={inter.className}>
         <Providers>
-          <Navbar />
+          {/* <Navbar /> */}
           <main className="min-h-screen bg-transparent font-sans text-slate-800 transition-colors duration-200 dark:text-slate-200">
             {children}
           </main>
